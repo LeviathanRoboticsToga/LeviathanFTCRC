186e58bc410d9eb179f617539a5934107dce1058
evanhuang <pumpkin.patch.evan@gmail.com>
2025-07-10 17:08:51 -0700

    5 :03

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLimeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLimeLight.java
@@ -2,8 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 
 @Autonomous
 public class TestLimeLight extends LinearOpMode {
